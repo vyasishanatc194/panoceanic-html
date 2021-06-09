e0b73439d5b888ed1963ded4e6bba2e90d100b8e
Ishanvyas <vyasishanatc194@gmail.com>
2021-06-09 20:37:00 +0530

    Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -160,6 +160,7 @@ $(document).ready(function () {
     },
   });
 
+  // buisness slider
   var owl = $(".owl-carousel-buisness").owlCarousel({
     loop: true,
     margin: 0,
@@ -180,9 +181,13 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
+        touchDrag: false,
+        mouseDrag: false,
       },
       768: {
         items: 3,
+        touchDrag: false,
+        mouseDrag: false,
       },
 
       1000: {
@@ -200,22 +205,16 @@ $(document).ready(function () {
   });
 
   function checkClasses() {
-    var total = $(
-      ".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active"
-    ).length;
+    var total = $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active").length;
 
-    $(
-      ".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item"
-    ).removeClass("firstActiveItem lastActiveItem");
+    $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item").removeClass("firstActiveItem lastActiveItem");
 
-    $(
-      ".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active"
-    ).each(function (index) {
-      if (index === 1) {
+    $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active").each(function (index) {
+      if (index === 0) {
         // this is the first one
         $(this).addClass("firstActiveItem center");
       }
-      if (index === total - 1 && total > 1) {
+      if (index === total - 0 && total > 0) {
         // this is the last one
         $(this).addClass("lastActiveItem");
       }
