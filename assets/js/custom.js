00e48a8c66fbe25f592c572eeafcda6f0f14adff
vyasishanatc194 <vyasishanatc194@gmail.com>
2021-06-21 15:38:40 +0530

    fix slider issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
   var wow = new WOW({
     // mobile: false,
   }).init();
-
+  var triggerEvents = false;
   /// smooth scroll
 
   $("a.smoth-scroll").on("click", function (e) {
@@ -165,12 +165,12 @@ $(document).ready(function () {
     loop: true,
     margin: 0,
     smartSpeed: 2000,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 10000,
     autoplayHoverPause: true,
     dots: false,
     dotsEach: true,
-    nav: false,
+    nav: true,
     // rewindNav : true,
     // items: 4,
     // slideBy: 2,
@@ -185,8 +185,9 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
-        touchDrag: false,
-        mouseDrag: false,
+        touchDrag: true,
+        nav: false,
+        mouseDrag: true,
       },
       768: {
         items: 3,
@@ -203,14 +204,31 @@ $(document).ready(function () {
     },
   });
 
+  $(".buisness-slides .owl-item").mouseover(function(){
+    $( ".buisness-slides .owl-item.center" ).each(function() {
+      $( this ).removeClass( "center" );
+    });
+    $(this).addClass("center");
+    var className = $('.buisness-slides .owl-item.center .item').attr('data-img-class');
+    $( ".buisness-slides .b-slide.active" ).each(function() {
+      $( this ).removeClass( "active" );
+    });
+    $( ".buisness-slides .b-slide."+className ).addClass( "active" );
+  });
+
+
   checkClasses();
   owl.on("translated.owl.carousel", function (event) {
-    checkClasses();
+    if(triggerEvents !== true){
+      checkClasses();
+    }    
   });
 
   function checkClasses() {
     var total = $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active").length;
-
+    $( ".buisness-slides .owl-item" ).each(function() {
+      $( this ).removeClass( "center" );
+    });
     $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item").removeClass("firstActiveItem lastActiveItem");
 
     $(".owl-carousel-buisness .owl-stage-outer .owl-stage .owl-item.active").each(function (index) {
@@ -231,63 +249,70 @@ $(document).ready(function () {
         $(this).addClass("activeItem3").removeClass("activeItem4");
 
         // new click
-        setInterval(function(){
-          var liObj = document.querySelectorAll(".owl-item.active");
+        // setInterval(function(){
+        //   var liObj = document.querySelectorAll(".owl-item.active");
           
-          for (var i = 0; i < liObj.length; i++) {
-              if(i === 4){
-              profileDetails = liObj[4];
+        //   for (var i = 0; i < liObj.length; i++) {
+        //       if(i === 4){
+        //       profileDetails = liObj[4];
               
-          ppp = profileDetails.querySelectorAll(
-                  ".btn-circle"
-                );
+        //   ppp = profileDetails.querySelectorAll(
+        //           ".btn-circle"
+        //         );
               
-              //ppp[0].classList.remove("mystyle");
-              ppp[0].classList.add("change-img-div");
-              ppp[0].onclick = function(){ ppp[0].click()};
-              ppp[0].removeEventListener("click", elemEventHandler , false);
-              liObj = {};
-              break;
-              //console.log(ppp[0].click());
-              }
-          } 
-      }, 100);
-      function elemEventHandler(){
-          console.log('event remoed');
-      }
+        //       //ppp[0].classList.remove("mystyle");
+        //       ppp[0].classList.add("change-img-div");
+        //       ppp[0].onclick = function(){ ppp[0].click()};
+        //       ppp[0].removeEventListener("click", elemEventHandler , false);
+        //       liObj = {};
+        //       break;
+        //       //console.log(ppp[0].click());
+        //       }
+        //   } 
+        // }, 100);
+        // function elemEventHandler(){
+        //   console.log('event remoed');
+        // }
       }
 
       if (index === 3) {
         $(this).addClass("activeItem4").removeClass("activeItem3");
 
         // new click
-        setInterval(function(){
-          var liObj = document.querySelectorAll(".owl-item.active");
+        // setInterval(function(){
+        //   var liObj = document.querySelectorAll(".owl-item.active");
           
-          for (var i = 0; i < liObj.length; i++) {
-              if(i === 5){
-              profileDetails = liObj[5];
+        //   for (var i = 0; i < liObj.length; i++) {
+        //       if(i === 5){
+        //       profileDetails = liObj[5];
               
-          ppp = profileDetails.querySelectorAll(
-                  ".btn-circle"
-                );
+        //   ppp = profileDetails.querySelectorAll(
+        //           ".btn-circle"
+        //         );
               
-              //ppp[0].classList.remove("mystyle");
-              ppp[0].classList.add("change-img-div");
-              ppp[0].onclick = function(){ ppp[0].click();ppp[0].click();};
-              ppp[0].removeEventListener("click", elemEventHandler , false);
-              liObj = {};
-              break;
-              //console.log(ppp[0].click());
-              }
-          } 
-        }, 100);
+        //       //ppp[0].classList.remove("mystyle");
+        //       ppp[0].classList.add("change-img-div");
+        //       ppp[0].onclick = function(){ ppp[0].click();ppp[0].click();};
+        //       ppp[0].removeEventListener("click", elemEventHandler , false);
+        //       liObj = {};
+        //       break;
+        //       //console.log(ppp[0].click());
+        //       }
+        //   } 
+        // }, 100);
       }
+      
+      var className = $('.buisness-slides .owl-item.center .item').attr('data-img-class');
+      $( ".buisness-slides .b-slide" ).each(function() {
+        $( this ).removeClass( "active" );
+      });
+      $( ".buisness-slides .b-slide."+className ).addClass( "active" );
     });
   }
 
 
   owl.on("changed.owl.carousel", function (property) {
+    
     var current = property.item.index;
     // var src = $(property.target).find(".owl-item").eq(current).find("img").attr('src');
     var src = $(property.target)
@@ -295,53 +320,85 @@ $(document).ready(function () {
       .eq(current)
       .find("div")
       .attr("class");
+
     // console.log('Image current is ' + src);
 
-    if (src == "item one") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-one").addClass("active");
-      $(".slide-one").parent().addClass("active-bx-1");
-    }
+      if (src == "item one") {
+        
+        $(".b-slide.active").removeClass("active");
+        $(".slide-one").addClass("active");
+        $(".slide-one").parent().addClass("active-bx-1");
+      }
 
-    if (src == "item two") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-two").addClass("active");
-      $(this).addClass("active-bx-2");
-    }
+      if (src == "item two") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-two").addClass("active");
+        $(this).addClass("active-bx-2");
+      }
 
-    if (src == "item three") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-three").addClass("active");
-      $(this).addClass("active-bx-3");
-    }
+      if (src == "item three") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-three").addClass("active");
+        $(this).addClass("active-bx-3");
+      }
 
-    if (src == "item four") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-four").addClass("active");
-      $(this).addClass("active-bx-4");
-    }
+      if (src == "item four") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-four").addClass("active");
+        $(this).addClass("active-bx-4");
+      }
 
-    if (src == "item five") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-five").addClass("active");
-    }
+      if (src == "item five") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-five").addClass("active");
+      }
 
-    if (src == "item six") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-six").addClass("active");
-    }
+      if (src == "item six") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-six").addClass("active");
+      }
 
-    if (src == "item seven") {
-      $(".b-slide.active").removeClass("active");
-      $(".slide-seven").addClass("active");
-    }
+      if (src == "item seven") {
+        $(".b-slide.active").removeClass("active");
+        $(".slide-seven").addClass("active");
+      }
   });
 
   $(".buisness-slides .buiness .buisness-box .btn.btn-circle").click(
-    function () {
-      owl.trigger("next.owl.carousel");
+    function () {   
+      triggerEvents = true;
+      var item = $(this).attr('data-item');
+      owl.trigger("next.owl.carousel");   
+      setTimeout(function(){ 
+       
+        var newitem = 0;
+        if(item !== "7"){
+          var newitem = parseInt(item)+1;
+        }else{
+          var newitem = 1;
+        }
+        $( ".buisness-slides .owl-item" ).each(function() {
+          $( this ).removeClass( "center" );
+        });
+        $("div").find(`[data-img-class='s-${newitem}']`).parent().addClass("center");
+        
+        var className = `s-${newitem}`;
+
+        $( ".buisness-slides .b-slide" ).each(function() {
+          $( this ).removeClass( "active" );
+        });
+        $( ".buisness-slides .b-slide."+className ).addClass( "active" );
+       // triggerEvents = false;
+      }, 1000);      
+     
     }
   );
+  $(".owl-next" ).click(function() {
+    triggerEvents = false;
+  });
+  $(".owl-prev" ).click(function() {
+    triggerEvents = false;
+  });
 
   // $('.owl-carousel-buisness .owl-dots .owl-dot').hover(function() {
 	// 	$(this).click();
